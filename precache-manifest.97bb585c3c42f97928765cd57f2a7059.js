bae6511ca95d7b85a4ba266a5160bda8864afec7
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-20 03:44:40 -0500

    Updates

---
+++ precache-manifest.97bb585c3c42f97928765cd57f2a7059.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "2df799579e2f6e71f862",
+    "url": "/VS_WebPortfolio/static/css/main.8b9104a0.chunk.css"
+  },
+  {
+    "revision": "2df799579e2f6e71f862",
+    "url": "/VS_WebPortfolio/static/js/main.2df79957.chunk.js"
+  },
+  {
+    "revision": "266190d1fb51ad12d0a4",
+    "url": "/VS_WebPortfolio/static/js/1.266190d1.chunk.js"
+  },
+  {
+    "revision": "a0e43bc28f09da1539be",
+    "url": "/VS_WebPortfolio/static/css/2.475280ae.chunk.css"
+  },
+  {
+    "revision": "a0e43bc28f09da1539be",
+    "url": "/VS_WebPortfolio/static/js/2.a0e43bc2.chunk.js"
+  },
+  {
+    "revision": "29ebda799286cfb15e32",
+    "url": "/VS_WebPortfolio/static/js/runtime~main.29ebda79.js"
+  },
+  {
+    "revision": "4325eb0f34548b9f7cc425c36f268f96",
+    "url": "/VS_WebPortfolio/static/media/mailz.4325eb0f.jpeg"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/VS_WebPortfolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/VS_WebPortfolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/VS_WebPortfolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/VS_WebPortfolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/VS_WebPortfolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/VS_WebPortfolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/VS_WebPortfolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "3d74a2b9c75b4756f165d6e00ce9fd4b",
+    "url": "/VS_WebPortfolio/static/media/profilephoto.3d74a2b9.jpg"
+  },
+  {
+    "revision": "e16d992710364c1c6ee1cf7ceca82253",
+    "url": "/VS_WebPortfolio/static/media/bg.e16d9927.jpeg"
+  },
+  {
+    "revision": "ac25f3029262de1acc3aa3063d8bb1db",
+    "url": "/VS_WebPortfolio/static/media/ProfilePic.ac25f302.png"
+  },
+  {
+    "revision": "f17b17eaf6cf60ec29b97c78afd4edbf",
+    "url": "/VS_WebPortfolio/static/media/education.f17b17ea.svg"
+  },
+  {
+    "revision": "77ae1b679453f8681c04d59c827838ce",
+    "url": "/VS_WebPortfolio/static/media/interests.77ae1b67.svg"
+  },
+  {
+    "revision": "0544df6cbc5b3a8844bce4aeb266e9f6",
+    "url": "/VS_WebPortfolio/static/media/programming-skills.0544df6c.svg"
+  },
+  {
+    "revision": "4940b2023affe10af0ba092555bf3e6c",
+    "url": "/VS_WebPortfolio/static/media/projects.4940b202.svg"
+  },
+  {
+    "revision": "a59c5d12dbc74e8698f847a01bca48c0",
+    "url": "/VS_WebPortfolio/static/media/work-history.a59c5d12.svg"
+  },
+  {
+    "revision": "03c4a214521537842b66f030ef28bc3e",
+    "url": "/VS_WebPortfolio/index.html"
+  }
+];
